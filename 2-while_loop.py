7ea7bb1da35fa6b8f7b97c05458a39de5e3ddc94
[Gordonkimera] <kasaggagordonkimera@gmail.com>
2022-10-19 22:19:18 +0300

    commite

---
+++ 2-while_loop.py
@@ -0,0 +1,8 @@
+# a while loop that counts from 0 to 10, and prints even numbers to the screen.
+x = 10
+i = 1
+
+while i <= x:
+    if i % 2 == 0:
+        print(i, end=" ")
+    i = i + 1
---
+++ 4-loop-continue.py
@@ -0,0 +1,13 @@
+for digit in "0165031806510":
+    if digit == "0":
+        digit = "X"
+    print(digit)
+   
+   
+   # REMOVE COMMENTS TO RUN CODE 
+
+# digit = (input("enter a long digits mixed with some zeros"))
+# for digit in digit:
+#     if digit == "0":
+#         digit = "X"
+#     print(digit)
